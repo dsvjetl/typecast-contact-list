795f21c9b4f4f19bbc7278307c7d07ec29b3077a
Domagoj Svjetličić <domagoj.svjetlicic@gmail.com>
2020-07-25 20:41:44 +0200

    responsive modal

--- src/components/blocks/Modal.vue
+++ src/components/blocks/Modal.vue
@@ -4,24 +4,33 @@
     v-if="showModal"
   >
     <div class="modal-window">
+
       <div class="modal-heading">
         <h3 class="title u-3 --bold-600">Delete</h3>
       </div>
+
       <div class="modal-body">
+
         <p class="message u-3">Are you sure you want to delete this contact?</p>
+
         <div class="actions-wrapper">
+
           <AppButton
+            class="button"
             :type="'cancel'"
             @click.native="handleNegativeClick"
           >
             <span class="u-3">Cancel</span>
           </AppButton>
+
           <AppButton
+            class="button"
             :type="'positive'"
             @click.native="handlePositiveClick"
           >
             <span class="u-3">Delete</span>
           </AppButton>
+
         </div>
       </div>
     </div>
@@ -37,8 +46,8 @@
     components: { AppButton },
   })
   export default class Modal extends Vue {
-    // TODO change to true
-    public showModal: boolean = false;
+    // TODO change to false
+    public showModal: boolean = true;
 
     public handleNegativeClick(): void {
       this.changeModalDisplay(false);
@@ -69,7 +78,11 @@
       background-color: $white;
       width: percentage(8 / 24);
       border-radius: 4px;
-      box-shadow: 0 0 14px 0 rgba($black, 0.27)
+      box-shadow: 0 0 14px 0 rgba($black, 0.27);
+
+      @include media('>=phone', '<tablet') {
+        width: percentage(22 / 24);
+      }
     }
 
     .modal-heading {
@@ -92,9 +105,24 @@
       padding: 30px;
     }
 
+    .button {
+      @include media('>=phone', '<custom1265') {
+        display: block;
+        margin: auto;
+
+        &:first-of-type{
+          margin: 0 auto 20px;
+        }
+      }
+    }
+
     .actions-wrapper {
       display: flex;
       justify-content: space-between;
+
+      @include media('>=phone', '<custom1265') {
+        display: block;
+      }
     }
   }
 </style>
